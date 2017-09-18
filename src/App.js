d95c4ca25fafdcadc74e25ee82057dee36c4858c
Adam Żaczek <adamzaczekul@gmail.com>
2017-09-19 01:04:41 +0200

    Display Table With Currencies

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchCurrencies } from './actions/fetchCurrencies';
 
+class Currency extends Component {
+  render() {
+    const { ask, bid, code } = this.props.currency;
+    return (
+      <tr>
+        <td>Ask: {ask}</td>
+        <td>Bid: {bid}</td>
+        <td>code: {code}</td>
+      </tr>
+    )
+  }
+}
+
+const ListCurrencies = (props) => {
+  const mapCurrencies = props.currencies.map(currency => <Currency key={currency.code} currency={currency} />)
+  return (
+    <table>
+      <th>ASK</th>
+      <th>BID</th>
+      <th>CODE</th>
+      {mapCurrencies}
+    </table>
+  );
+}
+
 class App extends Component {
 
   componentDidMount() {
@@ -14,7 +39,7 @@ class App extends Component {
         <div className="App-header">
           <h2>Choose yout favorite currencies</h2>
         </div>
-        The list will be here.
+        <ListCurrencies currencies={this.props.currencies} />
       </div>
     );
   }
